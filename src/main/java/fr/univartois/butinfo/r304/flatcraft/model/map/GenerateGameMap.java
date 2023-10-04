642c993bb9a2207b1d25310b550a4fcc62f9cb8c
Damien Allaert <damien_allaert@ens.univ-artois.fr>
2023-10-04 14:29:57 +0200

    implementation de la méthode pour générer la carte

--- src/main/java/fr/univartois/butinfo/r304/flatcraft/model/map/GenerateGameMap.java
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/map/GenerateGameMap.java
@@ -1,5 +1,43 @@
 package fr.univartois.butinfo.r304.flatcraft.model.map;
 
-public class GenerateGameMap{
+import fr.univartois.butinfo.r304.flatcraft.model.Cell;
+import fr.univartois.butinfo.r304.flatcraft.model.CellFactory;
+import fr.univartois.butinfo.r304.flatcraft.view.ISpriteStore;
 
+public class GenerateGameMap implements CellFactory {
+    int heigth;
+    int width;
+    int soilHeigth;
+    public GenerateGameMap(int heigth, int width, int soilHeigth){
+    }
+
+    @Override
+    public void setSpriteStore(ISpriteStore spriteStore) {
+
+    }
+
+    @Override
+    public Cell createSky() {
+        return null;
+    }
+
+    @Override
+    public Cell createSoilSurface() {
+        return null;
+    }
+
+    @Override
+    public Cell createSubSoil() {
+        return null;
+    }
+
+    @Override
+    public Cell createTrunk() {
+        return null;
+    }
+
+    @Override
+    public Cell createLeaves() {
+        return null;
+    }
 }
