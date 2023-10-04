152213a523230e2f99697b4a124be9a68f8aa50d
Damien Allaert <damien_allaert@ens.univ-artois.fr>
2023-10-04 14:51:50 +0200

    Insertion des cellules dans la carte

--- src/main/java/fr/univartois/butinfo/r304/flatcraft/model/map/GenerateGameMap.java
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/map/GenerateGameMap.java
@@ -4,40 +4,29 @@ import fr.univartois.butinfo.r304.flatcraft.model.Cell;
 import fr.univartois.butinfo.r304.flatcraft.model.CellFactory;
 import fr.univartois.butinfo.r304.flatcraft.view.ISpriteStore;
 
-public class GenerateGameMap implements CellFactory {
+import static java.lang.Math.round;
+
+public class GenerateGameMap{
     int heigth;
     int width;
     int soilHeigth;
+    CellFactory factory;
+    ISpriteStore spriteStore;
     public GenerateGameMap(int heigth, int width, int soilHeigth){
-    }
-
-    @Override
-    public void setSpriteStore(ISpriteStore spriteStore) {
-
-    }
-
-    @Override
-    public Cell createSky() {
-        return null;
-    }
-
-    @Override
-    public Cell createSoilSurface() {
-        return null;
-    }
-
-    @Override
-    public Cell createSubSoil() {
-        return null;
-    }
-
-    @Override
-    public Cell createTrunk() {
-        return null;
-    }
-
-    @Override
-    public Cell createLeaves() {
-        return null;
+        SimpleGameMap map = new SimpleGameMap(heigth,width,soilHeigth);
+        for(int i=0;i<heigth;i++){
+            for(int j=0;j<width;j++){
+                if(i<soilHeigth){
+                    Cell cell = factory.createSubSoil();
+                    map.setAt(i,j,cell);
+                } else if (i>round(heigth/1.5)) {
+                    Cell cell = factory.createSky();
+                    map.setAt(i,j,cell);
+                }else{
+                    Cell cell = factory.createSoilSurface();
+                    map.setAt(i,j,cell);
+                }
+            }
+        }
     }
 }
