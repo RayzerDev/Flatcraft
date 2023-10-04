7182edc21052264ae3e29f946065b3ca69a34946
Louis Karamucki <louis_karamucki@ens.univ-artois.fr>
2023-10-04 13:57:14 +0200

    Hérite de la classe AbstractMovable

--- src/main/java/fr/univartois/butinfo/r304/flatcraft/model/movables/Player.java
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/movables/Player.java
@@ -1,4 +1,18 @@
 package fr.univartois.butinfo.r304.flatcraft.model.movables;
 
-public class Player {
+import fr.univartois.butinfo.r304.flatcraft.model.FlatcraftGame;
+import fr.univartois.butinfo.r304.flatcraft.view.Sprite;
+
+public class Player extends AbstractMovable{
+    /**
+     * Crée une nouvelle instance de AbstractMovable.
+     *
+     * @param game      Le jeu dans lequel l'objet évolue.
+     * @param xPosition La position en x initiale de l'objet.
+     * @param yPosition La position en y initiale de l'objet.
+     * @param sprite    L'instance de {@link Sprite} représentant l'objet.
+     */
+    protected Player(FlatcraftGame game, double xPosition, double yPosition, Sprite sprite) {
+        super(game, xPosition, yPosition, sprite);
+    }
 }
