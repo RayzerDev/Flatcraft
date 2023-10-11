218861663f1c31f08fa0c89e3525730b960d7cee
Louis Karamucki <louis_karamucki@ens.univ-artois.fr>
2023-10-11 11:27:26 +0200

    Ajout ressources

--- src/main/java/fr/univartois/butinfo/r304/flatcraft/model/map/CellGridFactory.java
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/map/CellGridFactory.java
@@ -3,6 +3,7 @@ package fr.univartois.butinfo.r304.flatcraft.model.map;
 import fr.univartois.butinfo.r304.flatcraft.model.Cell;
 import fr.univartois.butinfo.r304.flatcraft.model.CellFactory;
 import fr.univartois.butinfo.r304.flatcraft.model.resources.Resource;
+import fr.univartois.butinfo.r304.flatcraft.model.resources.ToolType;
 import fr.univartois.butinfo.r304.flatcraft.view.ISpriteStore;
 import java.util.Random;
 
@@ -25,12 +26,12 @@ public class CellGridFactory implements CellFactory {
 
     @Override
     public Cell createSoilSurface() {
-        return new CellGrid(spriteStore.getSprite("grass"));
+        return new CellGrid(new Resource("grass",this.spriteStore.getSprite("grass"), ToolType.NO_TOOL,1));
     }
 
     @Override
     public Cell createSubSoil() {
-        return new CellGrid(this.spriteStore.getSprite("dirt"));
+        return new CellGrid(new Resource("dirt",this.spriteStore.getSprite("dirt"), ToolType.NO_TOOL,1));
     }
 
     @Override
