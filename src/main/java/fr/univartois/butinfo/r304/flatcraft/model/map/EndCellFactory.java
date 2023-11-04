3e6d3cce113ebd5cfb2ab70a3f933ddc8f8d8606
Anthony Rolland <anthony_rolland@ens.univ-artois.fr>
2023-11-04 19:10:14 +0100

    :zap: Classe pour l'END finie utilisée dans des cas rares

---
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/map/EndCellFactory.java
@@ -0,0 +1,53 @@
+package fr.univartois.butinfo.r304.flatcraft.model.map;
+
+import fr.univartois.butinfo.r304.flatcraft.model.Cell;
+import fr.univartois.butinfo.r304.flatcraft.model.CellFactory;
+import fr.univartois.butinfo.r304.flatcraft.model.resources.Resource;
+import fr.univartois.butinfo.r304.flatcraft.model.resources.ToolType;
+import fr.univartois.butinfo.r304.flatcraft.view.ISpriteStore;
+import fr.univartois.butinfo.r304.flatcraft.view.Sprite;
+
+
+public class EndCellFactory implements CellFactory {
+    ISpriteStore spriteStore;
+
+    @Override
+    public void setSpriteStore(ISpriteStore spriteStore) {
+        this.spriteStore = spriteStore;
+    }
+
+    @Override
+    public Cell createSky() {
+        return createCell("black_sky");
+    }
+
+    @Override
+    public Cell createSoilSurface() {
+        return createResourceCell("end_stone");
+    }
+
+    @Override
+    public Cell createSubSoil() {
+        return createResourceCell("dirt");
+    }
+
+    @Override
+    public Cell createTrunk() {
+        return createCell("tree");
+    }
+
+    @Override
+    public Cell createLeaves() {
+        return createCell("leaves");
+    }
+
+    private Cell createCell(String name) {
+        Sprite sprite = spriteStore.getSprite(name);
+        return new CellGrid(sprite);
+    }
+
+    private Cell createResourceCell(String name) {
+        Sprite sprite = spriteStore.getSprite(name);
+        return new CellGrid(new Resource(name, sprite, ToolType.NO_TOOL, 1));
+    }
+}
