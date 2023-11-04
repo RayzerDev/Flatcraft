53fe6563adf3d3227a7c69c11dba763104e0853e
Anthony Rolland <anthony_rolland@ens.univ-artois.fr>
2023-11-04 18:44:59 +0100

    :zap: Ajout de la classe Nether pour créer le Nether selon l'abstractFactory

---
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/map/NetherCellFactory.java
@@ -0,0 +1,60 @@
+package fr.univartois.butinfo.r304.flatcraft.model.map;
+
+import fr.univartois.butinfo.r304.flatcraft.model.Cell;
+import fr.univartois.butinfo.r304.flatcraft.model.CellFactory;
+import fr.univartois.butinfo.r304.flatcraft.model.resources.Resource;
+import fr.univartois.butinfo.r304.flatcraft.model.resources.ToolType;
+import fr.univartois.butinfo.r304.flatcraft.view.ISpriteStore;
+import fr.univartois.butinfo.r304.flatcraft.view.Sprite;
+
+import java.util.Random;
+
+public class NetherCellFactory implements CellFactory {
+
+    private final Random RANDOM = new Random();
+    ISpriteStore spriteStore;
+
+    @Override
+    public void setSpriteStore(ISpriteStore spriteStore) {
+        this.spriteStore = spriteStore;
+    }
+
+    @Override
+    public Cell createSky() {
+        return createCell("netherrack");
+    }
+
+    @Override
+    public Cell createSoilSurface() {
+        if (RANDOM.nextInt(10) < 4) {
+            return createResourceCell("netherrack");
+        }
+
+        return createResourceCell("lava");
+    }
+
+    @Override
+    public Cell createSubSoil() {
+        return createResourceCell("dirt");
+    }
+
+    @Override
+    public Cell createTrunk() {
+        return createCell("tree");
+    }
+
+    @Override
+    public Cell createLeaves() {
+        return createCell("leaves");
+    }
+
+    private Cell createCell(String name) {
+        Sprite sprite = spriteStore.getSprite(name);
+        return new CellGrid(sprite);
+    }
+
+    private Cell createResourceCell(String name) {
+        Sprite sprite = spriteStore.getSprite(name);
+        return new CellGrid(new Resource(name, sprite, ToolType.NO_TOOL, 1));
+    }
+}
