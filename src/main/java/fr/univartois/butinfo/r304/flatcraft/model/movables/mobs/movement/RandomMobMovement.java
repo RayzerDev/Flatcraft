55b246ce22c213fce048281d652efd4bb3eba083
Louis Karamucki <louis_karamucki@ens.univ-artois.fr>
2023-11-05 23:10:37 +0100

    Implementation du mouvement aléatoire

--- src/main/java/fr/univartois/butinfo/r304/flatcraft/model/movables/mobs/movement/RandomMobMovement.java
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/movables/mobs/movement/RandomMobMovement.java
@@ -1,11 +1,12 @@
 package fr.univartois.butinfo.r304.flatcraft.model.movables.mobs.movement;
 
-import fr.univartois.butinfo.r304.flatcraft.model.IMovable;
+import java.util.Random;
 import fr.univartois.butinfo.r304.flatcraft.model.movables.mobs.AbstractMob;
 
 public final class RandomMobMovement implements IMobMovementStrategy {
+    private static final Random random = new Random();
     @Override
     public void move(AbstractMob mob) {
-
+        mob.setHorizontalSpeed(random.nextDouble(-150,150));
     }
 }
