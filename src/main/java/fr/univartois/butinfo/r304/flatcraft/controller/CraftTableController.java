93836047981ab617e7d6561c0bcb8f88028fed5b
Romain Wallon <romain.wallon@univ-artois.fr>
2023-11-21 18:51:08 +0100

    :sparkles: Implémente le glisser-déposer pour la table de craft.

--- src/main/java/fr/univartois/butinfo/r304/flatcraft/controller/CraftTableController.java
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/controller/CraftTableController.java
@@ -16,11 +16,17 @@
 
 package fr.univartois.butinfo.r304.flatcraft.controller;
 
+import java.util.Optional;
+
 import fr.univartois.butinfo.r304.flatcraft.model.FlatcraftGame;
 import fr.univartois.butinfo.r304.flatcraft.model.resources.Resource;
 import javafx.fxml.FXML;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.GridPane;
 
 /**
@@ -70,13 +76,19 @@ public final class CraftTableController {
      * Le bouton permettant d'effacer les ressources ajoutées à la table de craft.
      */
     @FXML
-    private Button clearButton;
+    private Button craftButton;
 
     /**
      * Le bouton permettant d'ajouter la ressource produite à l'inventaire du joueur.
      */
     @FXML
     private Button addButton;
+
+    /**
+     * Le bouton permettant d'effacer les ressources ajoutées à la table de craft.
+     */
+    @FXML
+    private Button clearButton;
 
     /**
      * Initialise ce contrôleur.
@@ -93,15 +105,86 @@ public final class CraftTableController {
                 resourceViews[i][j] = new ImageView();
 
                 // On configure la taille appropriée pour la vue.
-                resourceViews[i][j].minWidth(16);
-                resourceViews[i][j].maxWidth(16);
-                resourceViews[i][j].minHeight(16);
-                resourceViews[i][j].maxHeight(16);
+                resourceViews[i][j].setFitHeight(16);
+                resourceViews[i][j].setFitWidth(16);
+
+                // On positionne la vue correctement.
+                GridPane.setHalignment(resourceViews[i][j], HPos.CENTER);
+                GridPane.setValignment(resourceViews[i][j], VPos.CENTER);
+                resourceViews[i][j].setPickOnBounds(true);
+
+                // On ajoute la possibilité de déposer des ressource sur la grille.
+                dropResource(resourceViews[i][j], i, j);
 
                 // On ajoute la vue à la grille.
                 craftGrid.add(resourceViews[i][j], j, i);
             }
         }
+    }
+
+    /**
+     * Dépose (ou pas) une ressource sur un emplacement de la grille.
+     *
+     * @param imageView La vue affichant l'image de la ressource.
+     * @param row La ligne où la ressource a été déposée.
+     * @param column La colonne où la ressource a été déposée.
+     */
+    private void dropResource(ImageView imageView, int row, int column) {
+        // On accepte tous les modes de transfert.
+        imageView.setOnDragOver(event -> {
+            if ((event.getGestureSource() != imageView) &&
+                    event.getDragboard().hasString() && event.getDragboard().hasImage()) {
+                event.acceptTransferModes(TransferMode.ANY);
+            }
+            event.consume();
+        });
+
+        // On affiche une prévisualisation du dépôt lorsque l'on survole la case.
+        imageView.setOnDragEntered(event -> {
+            if ((event.getGestureSource() != imageView) &&
+                    event.getDragboard().hasString() && event.getDragboard().hasImage()) {
+                imageView.setImage(event.getDragboard().getImage());
+                imageView.setOpacity(0.2);
+            }
+            event.consume();
+        });
+
+        // On dépose la ressource au moment voulu.
+        imageView.setOnDragDropped(event -> {
+            Dragboard dragboard = event.getDragboard();
+            boolean success = false;
+
+            if (dragboard.hasString() && dragboard.hasImage()) {
+                // TODO Remplacez cette affectation par la récupération de la ressource dans l'inventaire du joueur.
+                Optional<Resource> resource = Optional.empty();
+                if (resource.isPresent()) {
+                    resources[row][column] = resource.get();
+                    craftButton.setDisable(false);
+                    clearButton.setDisable(false);
+                    success = true;
+                }
+            }
+
+            event.setDropCompleted(success);
+            event.consume();
+        });
+
+        // On remet la vue dans son état d'origine après le survol.
+        imageView.setOnDragExited(event -> {
+            if (resources[row][column] == null) {
+                imageView.setImage(null);
+            } else {
+                imageView.setImage(resources[row][column].getSprite().getImage());
+            }
+            imageView.setOpacity(1);
+            event.consume();
+        });
+
+        // Lorsque la ressource est déposée, elle est retirée de l'inventaire du joueur.
+        imageView.setOnDragDone(event -> {
+            // TODO Retirez de l'inventaire du joueur la ressource ayant été déposée.
+            event.consume();
+        });
     }
 
     /**
@@ -125,6 +208,7 @@ public final class CraftTableController {
         // On met à jour les actions disponibles.
         addButton.setDisable(false);
         craftGrid.setDisable(true);
+        craftButton.setDisable(true);
         clearButton.setDisable(true);
     }
 
@@ -133,7 +217,7 @@ public final class CraftTableController {
      */
     @FXML
     private void addToInventory() {
-        // TODO Récupérer le joueur ou définir une méthode pour pouvoir effectuer l'ajout.
+        // TODO Ajoutez un l'inventaire du joueur la ressource "product" ayant été produite.
     }
 
     /**
@@ -144,13 +228,14 @@ public final class CraftTableController {
         // On retire toutes les ressources déposées sur la table de craft.
         for (int i = 0; i < resources.length; i++) {
             for (int j = 0; j < resources[i].length; j++) {
-                // TODO Remettre les ressources non utilisée dans l'inventaire.
+                // TODO Remettez les ressources non null dans l'inventaire du joueur.
                 resources[i][j] = null;
                 resourceViews[i][j].setImage(null);
             }
         }
 
         // On met à jour les actions disponibles.
+        craftButton.setDisable(true);
         clearButton.setDisable(true);
     }
 
