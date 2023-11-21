55bf17be9df23519bb30d2cc836845560e2502c5
Romain Wallon <romain.wallon@univ-artois.fr>
2023-11-21 14:57:08 +0100

    :sparkles: Ajoute les méthodes permettant d'utiliser la table de craft et le fourneau.

--- src/main/java/fr/univartois/butinfo/r304/flatcraft/model/FlatcraftGame.java
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/FlatcraftGame.java
@@ -19,6 +19,7 @@ package fr.univartois.butinfo.r304.flatcraft.model;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import fr.univartois.butinfo.r304.flatcraft.model.resources.Resource;
 import fr.univartois.butinfo.r304.flatcraft.view.ISpriteStore;
 import fr.univartois.butinfo.r304.flatcraft.view.Sprite;
 import javafx.beans.property.IntegerProperty;
@@ -279,6 +280,33 @@ public final class FlatcraftGame {
 
         // On récupère enfin la cellule à cette position dans la carte.
         return map.getAt(row, column);
+    }
+
+    /**
+     * Crée une nouvelle ressource à l'aide d'un ensemble de ressources, en suivant les
+     * règles de la table de craft.
+     *
+     * @param inputResources Les ressources déposées sur la table de craft.
+     *
+     * @return La ressource produite.
+     */
+    public Resource craft(Resource[][] inputResources) {
+        // TODO Vous devez compléter cette méthode.
+        throw new UnsupportedOperationException("Pas encore implémentée !");
+    }
+
+    /**
+     * Crée une nouvelle ressource à l'aide d'un combustible et d'une ressource, en suivant les
+     * règles du fourneau.
+     *
+     * @param fuel Le matériau combustible utilisé dans le fourneau.
+     * @param resource La ressource à transformer.
+     *
+     * @return La ressource produite.
+     */
+    public Resource cook(Resource fuel, Resource resource) {
+        // TODO Vous devez compléter cette méthode.
+        throw new UnsupportedOperationException("Pas encore implémentée !");
     }
 
 }
