ba55fcab899e428f6552f2bec883321ab26a35e4
Anthony Rolland <anthony_rolland@ens.univ-artois.fr>
2023-11-29 14:56:57 +0100

    :sparkles: Ajout de l'interface pour les méthode qui crééent la règle étape par étape

---
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/craft/IBuilder.java
@@ -0,0 +1,10 @@
+package fr.univartois.butinfo.r304.flatcraft.model.craft;
+
+public interface IBuilder {
+
+    void buildRule(String rule);
+
+    void buildProduct(String product);
+
+    void buildQuantite(int quantite);
+}
