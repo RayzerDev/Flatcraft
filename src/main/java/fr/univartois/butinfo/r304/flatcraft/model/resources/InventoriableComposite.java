fb6de0e7a65a6f32ce91eca36a04b24314e96381
Anthony Rolland <anthony_rolland@ens.univ-artois.fr>
2023-12-13 14:02:43 +0100

    :sparkles: Implémentation des méthodes d'Inventoriable

--- src/main/java/fr/univartois/butinfo/r304/flatcraft/model/resources/InventoriableComposite.java
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/resources/InventoriableComposite.java
@@ -2,19 +2,46 @@ package fr.univartois.butinfo.r304.flatcraft.model.resources;
 
 import fr.univartois.butinfo.r304.flatcraft.view.Sprite;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.logging.Logger;
+
 public class InventoriableComposite implements Inventoriable{
+
+    private final String nom;
+
+    private final List<Inventoriable> composants;
+
+    public InventoriableComposite(String nom) {
+        this.nom = nom;
+        this.composants = new ArrayList<>();
+    }
+
     @Override
     public String getName() {
-        return null;
+        return nom;
     }
 
     @Override
     public Sprite getSprite() {
+        for (Inventoriable composant : composants) {
+            if (composant.getSprite() != null) {
+                return composant.getSprite();
+            }
+        }
+        System.out.println("Sprite non trouvé");
         return null;
     }
 
     @Override
     public ToolType getToolType() {
+        for (Inventoriable composant : composants) {
+            if (composant.getToolType() != null) {
+                return composant.getToolType();
+            }
+        }
+        System.out.println("ToolType non trouvé");
         return null;
     }
 }
